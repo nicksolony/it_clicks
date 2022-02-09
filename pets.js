ff556cb7764fb9e0cde098d564902fe05abe1e5c
nicksolony <nicksolony@gmail.com>
2022-02-08 21:18:56 -0500

    added pets.js

---
+++ pets.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let pets = [
+    {
+      name: "Fluffy",
+      type: "Cat 🐈",
+      photoUrl: "https://pet-ratings.surge.sh/fluffy.webp",
+      comments: "So cute! 😻",
+      numLikes: 2,
+    },
+    {
+      name: "Dr. Slitherscale",
+      type: "Snake 🐍",
+      photoUrl: "https://pet-ratings.surge.sh/slitherscale.webp",
+      comments: "Hissssss....",
+      numLikes: 1,
+    },
+    {
+      name: "Spike",
+      type: "Porcupine 🦔",
+      photoUrl: "https://pet-ratings.surge.sh/spike.webp",
+      comments: "He loves to be petted!",
+      numLikes: 0,
+    },
+  ];
+  
+
